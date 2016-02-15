3c8a21ffb1e0266ad9bafe21bbd2877cb7c1952e
Gerrit <gerrityntema@gmail.com>
2016-02-15 12:11:53 -0800

    Add postcard create view and controller structure

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,16 @@
 angular.module('mailthat', [
-	'mailthat.login',
-	'ngRoute'
+  'mailthat.login',
+  'ngRoute'
 ])
 .config(function ($routeProvider, $httpProvider) {
   $routeProvider
     .when('/login', {
       templateUrl: 'app/auth/login.html',
       controller: 'LoginCtrl'
+    })
+    .when('/handleauth', {
+      templateUrl: 'app/auth/login.html',
+      controller: 'CreateCtrl',
     })
     .otherwise({
       redirectTo: '/'
---
+++ client/app/create/create.js
@@ -0,0 +1,4 @@
+angular.module('mailthat.create', [])
+.controller('CreateCtrl', function($scope) {
+  
+});
