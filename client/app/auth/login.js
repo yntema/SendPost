5ad233f64287592744983aaf29d7516c74c32bf0
Gerrit <gerrityntema@gmail.com>
2016-04-05 15:23:28 -0700

    Add www subdomain to redirect

--- client/app/auth/login.js
+++ client/app/auth/login.js
@@ -1,5 +1,5 @@
 angular.module('mailthat.login', [])
 .controller('LoginCtrl', function($scope) {
   $scope.client_id = 'b6f67f16460b4bfd9a922a85276b5e4d';
-  $scope.redirect_uri = 'http://sendpost.co/';
+  $scope.redirect_uri = 'http://www.sendpost.co/';
 })
