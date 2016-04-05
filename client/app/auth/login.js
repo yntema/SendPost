94814530fb7a99167620dadd45d0e4024b66acf0
Gerrit <gerrityntema@gmail.com>
2016-04-05 15:16:51 -0700

    Update redirect uri

--- client/app/auth/login.js
+++ client/app/auth/login.js
@@ -1,5 +1,5 @@
 angular.module('mailthat.login', [])
 .controller('LoginCtrl', function($scope) {
   $scope.client_id = 'b6f67f16460b4bfd9a922a85276b5e4d';
-  $scope.redirect_uri = 'http://localhost:3000/';
+  $scope.redirect_uri = 'http://sendpost.co/';
 })
